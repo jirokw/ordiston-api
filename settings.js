e2fda9d7ed68fb343342dd75cb39cffef9c239fa
jirokw <155631154+jirokw@users.noreply.github.com>
2024-01-15 19:25:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
-global.creator = 'eabdalmufid'  //Isi creator 
-global.MONGO_DB_URI = ""        //Isi Database Mongodb
+global.creator = 'abcdejiro'  //Isi creator 
+global.MONGO_DB_URI = "mongodb+srv://Jiroofc:S7iikxm6QKTDuFSS@cluster0.jia1yhc.mongodb.net/?retryWrites=true&w=majority"        //Isi Database Mongodb
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //Isi apa aja bebas
-global.your_email = ""          //Isi Email Untuk Pengiriman Kode
-global.email_password = ""      //Isi Aplikasi Sandi Email Pengeriman Kode
+global.your_email = "apimarketplace09@gmail.com"          //Isi Email Untuk Pengiriman Kode
+global.email_password = "szlq upxq bagf ksjf"      //Isi Aplikasi Sandi Email Pengeriman Kode
 global.limitCount = 100
-global.YUOR_PORT = 8000
+global.YUOR_PORT = 8080
 global.loghandler = {
     noapikey: {
         status: 403,
